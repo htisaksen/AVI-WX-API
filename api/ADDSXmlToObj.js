b709d8e267f5747591baa831859ddbd16fa7a60d
Hyosun Takaaki Isaksen <ttkay123@gmail.com>
2017-02-09 23:07:33 -0500

    added promise

--- api/ADDSXmlToObj.js
+++ api/ADDSXmlToObj.js
@@ -6,6 +6,8 @@
   var parser = new xml2js.Parser();
 // Requires --------------------------------------------------------------------
 
+url = 'https://aviationweather.gov/adds/dataserver_current/httpparam?datasource=metars&requestType=retrieve&format=xml&mostRecentForEachStation=constraint&hoursBeforeNow=1.25&stationString=KFRG'
+
 parser.on('error', function(err) { console.log('Parser error', err); });
 
 // Inits------------------------------------------------------------------------
@@ -15,17 +17,32 @@ parser.on('error', function(err) { console.log('Parser error', err); });
 // Inits------------------------------------------------------------------------
 
 
-  https.get(xmlUrl, function(res) {
+  // https.get(url, function(res) {
+  //     if (res.statusCode >= 200 && res.statusCode < 400) {
+  //       res.on('data', function(data_) {
+  //          data += data_.toString();
+  //       });
+  //       res.on('end', function() {
+  //         parser.parseString(data, function(err, result) {
+  //           console.log(data)
+  //           console.log('FINISHED', err, result);
+  //         });
+  //       });
+  //     }
+  // });
+
+  var parseStringAsync = Promise.promisify(parser.parseString);
+
+  https.get(url, function(res) {
       if (res.statusCode >= 200 && res.statusCode < 400) {
         res.on('data', function(data_) {
            data += data_.toString();
         });
         res.on('end', function() {
           parser.parseString(data, function(err, result) {
+            console.log(data)
             console.log('FINISHED', err, result);
-            var passThrough = result;
           });
         });
       }
   });
-  return passThrough
--- api/Avi-WX-API.js
+++ api/Avi-WX-API.js
@@ -1,4 +1,6 @@
-var getXml = require('./getXmlToObj.js')
+var getXml = require('./getXmlToObj');
+var Promise = require("bluebird");
+
 
 url = 'https://aviationweather.gov/adds/dataserver_current/httpparam?datasource=metars&requestType=retrieve&format=xml&mostRecentForEachStation=constraint&hoursBeforeNow=1.25&stationString=KFRG'
 
