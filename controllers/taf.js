14d26da16a780c01a24b81c3915dea0bb0fb3785
Hyosun Takaaki Isaksen <ttkay123@gmail.com>
2017-02-28 20:45:55 -0500

    cleaned up TAF for most recent taf

--- controllers/taf.js
+++ controllers/taf.js
@@ -5,7 +5,7 @@ var express = require("express"),
 //Grabs most recent taf data with provided Airport ICAO code.
 router.get('/:airportId',function(req, res){
   airport = req.params.airportId
-  addsEndpoint = "https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=xml&hoursBeforeNow=0&stationString="+airport
+  addsEndpoint = "https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=xml&hoursBeforeNow=3&timeType=issue&mostRecent=true&stationString="+airport
   return getXml(addsEndpoint, function(json){
     if(json.response.data[0].TAF){
       res.json(json.response.data[0].TAF);
