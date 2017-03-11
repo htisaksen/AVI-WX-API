2df71b52f63aebed0d378f5bf1c4a1ff94100ae2
Hyosun Takaaki Isaksen <ttkay123@gmail.com>
2017-03-11 12:55:05 -0500

    changed readme, added root route message

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 var express = require("express"),
     router = express.Router();
 
+router.get('/',function(req,res){
+  res.send('Please use /api/metar/(ICAO Code)  OR  /api/taf/(ICAO Code)')
+})
+
 router.use('/api/metar', require('./metar'));
 router.use('/api/taf', require('./taf'));
 
--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.set('json spaces', 2);
 // Routes are in controllers
 app.use(require('./controllers'));
 
-app.listen('3000', function(){
-  console.log('Listening on port 3000')
+app.listen('3005', function(){
+  console.log('Listening on port 3005')
 });
